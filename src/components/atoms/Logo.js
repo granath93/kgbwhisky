adf29758a310cdb758ddbe985da40e1cd91b7751
Amanda Granath <granath93@gmail.com>
2022-05-21 22:20:57 +0200

    update some errors

--- src/components/atoms/Logo.js
+++ src/components/atoms/Logo.js
@@ -1,7 +1,9 @@
+import Image from 'next/image';
+
 const Logo = ({ height }) => {
   const width = height * 1.54;
 
-  return <img height={height} width={width} src="/logga-gul.svg" />;
+  return <Image alt="" height={height} width={width} src="/logga-gul.svg" />;
 };
 
 export default Logo;
